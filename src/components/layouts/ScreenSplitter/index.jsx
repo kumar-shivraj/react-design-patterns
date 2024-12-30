ae0170b6ef4dece6b21c3b36758792724b22802f
kumar-shivraj <shivrajkumar11math@gmail.com>
2024-12-30 17:22:30 +0530

    screen splitter : put SplitScreen component in any place, it should work irrespective of it's placement in the page

--- src/components/layouts/ScreenSplitter/index.jsx
+++ src/components/layouts/ScreenSplitter/index.jsx
@@ -3,7 +3,12 @@ import Right from "./Right";
 import SplitScreen from "./SplitScreen";
 
 const ScreenSplitter = () => {
-  return <SplitScreen Left={Left} Right={Right} />;
+  return (
+    <div className="absolute top-1/2 left-1/2 h-1/2 w-1/2 translate-x-[-50%] translate-y-[-50%] text-white  bg-slate-600">
+      <SplitScreen Left={Left} Right={Right} />
+    </div>
+  );
+  // return <SplitScreen Left={Left} Right={Right} />;
 };
 
 export default ScreenSplitter;
